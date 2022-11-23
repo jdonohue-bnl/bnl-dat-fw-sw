0917ae12dc0c19c6bbf7b0ff508a33df8296473f
Shan Gao <gao33.bnl@gmail.com>
2022-11-23 16:01:17 -0500

    for FW released on 11122022

--- chkout_no_mon.py
+++ chkout_no_mon.py
@@ -7,7 +7,7 @@ import numpy as np
 import pickle
 import copy
 import os
-from tools import ana_tools
+from QC_tools import QC_tools
 from fpdf import FPDF
 
 def CreateFolders(fembNo, env, toytpc):
@@ -64,7 +64,7 @@ def CreateFolders(fembNo, env, toytpc):
 
 def GenReport(fembNo, rawdata, pwr_meas, mon_refs, mon_temps, mon_adcs, logs, PLOTDIR, nchips):
 
-    qc_tools = ana_tools()
+    qc_tools = QC_tools()
    
     femb_list = [int(ifemb[-1]) for ifemb,_ in fembNo.items()]
     print(femb_list)
@@ -184,14 +184,15 @@ t1 = time.time()
 ####### Power and configue FEMBs #######
 chk = WIB_CFGS()
 chk.wib_fw()
+chk.wib_timing(pll=True, fp1_ptc0_sel=0, cmd_stamp_sync = 0x0)
 
 #set FEMB voltages
 chk.fembs_vol_set(vfe=3.0, vcd=3.0, vadc=3.5)
 
 #power on FEMBs
-#chk.femb_powering(fembs)
-#time.sleep(2)
-#pwr_meas = chk.get_sensors()
+chk.femb_powering(fembs)
+time.sleep(2)
+pwr_meas = chk.get_sensors()
 chk.femb_cd_rst()
 
 snc = 1 # 200 mV
@@ -217,7 +218,6 @@ for femb_id in fembs:
     cfg_paras_rec.append( (femb_id, copy.deepcopy(chk.adcs_paras), copy.deepcopy(chk.regs_int8), adac_pls_en) )
     chk.femb_cfg(femb_id, adac_pls_en )
 
-chk.data_align(fembs)
 time.sleep(0.5)
 
 ####### Take data #######
@@ -265,8 +265,8 @@ else:
     nchips=[0,4]
 
 ####### Power off FEMBs #######
-#print("Turning off FEMBs")
-#chk.femb_powering([])
+print("Turning off FEMBs")
+chk.femb_powering([])
 
 ####### Generate report #######
 if save:
