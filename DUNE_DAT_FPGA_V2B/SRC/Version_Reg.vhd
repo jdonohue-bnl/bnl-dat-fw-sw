6db90e71b2cc7d400532a770be7c80b7795c0cc3
jdonohue-bnl <jdonohue@bnl.gov>
2023-03-22 18:54:00 -0400

    Removed STP probes, compiled bitfiles

--- DUNE_DAT_FPGA_V2B/SRC/Version_Reg.vhd
+++ DUNE_DAT_FPGA_V2B/SRC/Version_Reg.vhd
@@ -9,7 +9,7 @@ ENTITY version_reg IS
 END version_reg;
 ARCHITECTURE rtl OF version_reg IS
 BEGIN
-    data_out <= X"224";
-    Date_s <= X"20230310";
-    Time_s <= X"195005";     
+    data_out <= X"226";
+    Date_s <= X"20230322";
+    Time_s <= X"185043";     
 END rtl;
